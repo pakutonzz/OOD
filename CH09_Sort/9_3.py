688c018dae70c0c6c2a5f4086fa2b51180c10af2
pakutonzz <patkultornchairat@gmail.com>
2024-09-26 14:27:10 +0700

    9

--- CH09_Sort/9_3.py
+++ CH09_Sort/9_3.py
@@ -7,16 +7,18 @@ def insert(arr, n):
     last = arr[n - 1]
     j = n - 2
 
-    while j >= 0 and arr[j] > last:
+    def shift_elements(arr, j, last):
+        if j < 0 or arr[j] <= last:
+            arr[j + 1] = last
+            if n == len(arr):
+                print(f"insert {last} at index {j + 1} : {arr[:n]}")
+            else:
+                print(f"insert {last} at index {j + 1} : {arr[:n]} {arr[n:]}")
+            return
         arr[j + 1] = arr[j]
-        j -= 1
-    
-    arr[j + 1] = last
+        shift_elements(arr, j - 1, last)
 
-    if n == len(arr):
-        print(f"insert {last} at index {j + 1} : {arr[:n]}")
-    else:
-        print(f"insert {last} at index {j + 1} : {arr[:n]} {arr[n:]}")
+    shift_elements(arr, j, last)
 
 def recursive_insertion_sort(arr, n=None):
     if n is None:
